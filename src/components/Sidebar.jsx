d8bec7422028ec62e7b59c2809f9a7b0f5318ab1
Russ <136340000+rushil959@users.noreply.github.com>
2024-02-15 15:02:29 +0530

    Added exercise description

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,11 @@
+// https://raw.githubusercontent.com/rushil959/mock-data/main/nav-items.json
+//1) Fetch the data from the link and use the data to create a sidebar component which will render a list of navitems. 
+//2) Each navitem represents a file/folder.
+//3) The folders should be expanded by default and should toggle between expanded and collapsed state on click.
+//4) All the files/folders listed in a folder should have some space on the left to clearly indicate the hierarchical structure.
+//Assume that the number of levels of nesting is not fixed. The solution should support any level of nesting.
+
 export default function Sidebar() {
   return <div className="sidebar">Sidebar</div>;
 }
-// https://raw.githubusercontent.com/rushil959/mock-data/main/nav-items.json
+
